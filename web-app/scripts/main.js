afcc3497e916bb1afefde728d07fc2d4a5949930
Ben Holzhauer <33013768+benholzhauer@users.noreply.github.com>
2018-04-20 11:47:18 -0400

    Web app complete with working PHP server

--- web-app/scripts/main.js
+++ web-app/scripts/main.js
@@ -3,147 +3,286 @@
 // Class: CS 405G-001
 
 window.onload = function () {
+  // Define user UID and role
+  var uid;
+  var role;
 
-    // Get session data from server to check if user is logged in
-    $.get("server.php", function (data) {
-        // Check if user is logged in
-        if (data) {
-            // Check if user is manager
-            if (data == "manager") {
-                // Show manager options in navigation bar and on main page
-                $("#manageNav").show();
-                $("#manageLink").show();
-            }
-            else {
-                // Redirect to home if trying to use manager options as customer
-                if (window.location.href.indexOf("manage") >= 0) {
-                    window.location.href = "index.html";
+  // Get session data from server to check if user is logged in
+  $.get("server.php", function (data) {
+    console.log(data);
+
+    // Store user UID and role
+    uid = JSON.parse(data)[0];
+    role = JSON.parse(data)[1];
+
+    // Check if UID and role exist
+    if (uid && role) {
+      // Check if user is manager
+      if (role == "manager") {
+        // Show manager options in navigation bar and on main page
+        $("#manageNav").show();
+        $("#manageLink").show();
+      } else {
+        // Redirect to home if trying to use manager options as customer
+        if (window.location.href.indexOf("manage") >= 0) {
+          window.location.href = "index.html";
+        }
+      }
+
+      $("#features").show(); // Show user options in navigation bar
+      $("#login").hide(); // Hide log in content from main page
+      $("#choices").show(); // Show user options on main page
+      $("#loginNav").hide(); // Hide log in link
+      $("#signupNav").hide(); // Hide sign up link
+      $("#logoutNav").show(); // Show log out link
+
+      // Redirect to home if trying to sign up when already logged in
+      if (window.location.href.indexOf("signup") >= 0) {
+        window.location.href = "index.html";
+      }
+    } else {
+      // Redirect to home if trying to use options when logged out
+      if (
+        window.location.href.indexOf("search") >= 0 ||
+        window.location.href.indexOf("order") >= 0 ||
+        window.location.href.indexOf("review") >= 0 ||
+        window.location.href.indexOf("history") >= 0 ||
+        window.location.href.indexOf("manage") >= 0
+      ) {
+        window.location.href = "index.html";
+      }
+    }
+  });
+
+  // Check for log in form submissions
+  $("#login form").submit(function () {
+    // Send log in data to server
+    $.ajax({
+      type: "POST",
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+      success: function (response) {
+        console.log(response);
+
+        window.location.href = "index.html";
+      },
+    });
+    return false;
+  });
+
+  // Check for sign up form submissions
+  $("#signup form").submit(function () {
+    // Send sign up data to server
+    $.ajax({
+      type: "POST",
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+      success: function (response) {
+        console.log(response);
+
+        window.location.href = "index.html";
+      },
+    });
+    return false;
+  });
+
+  // Check if search term is changed to date
+  $("#search form select").click(function() {
+  if ($(this).find("option:selected").val() == "b.pubDate") {
+	 // Change search word input to date
+    $("#search form input[name=searchWords]").attr("type", "date");
+  }
+	});
+
+  // Check for search form submissions
+  $("#search form").submit(function (e) {
+    // Show results table
+    $("#results").show();
+
+    // Highlight search term in results table
+    var searchTerm = $(this).find("option:selected").val();
+    $("#results #" + searchTerm).addClass("text-primary");
+
+    // Disable new submissions and changing search term
+    $(this).find(":submit").prop("disabled", true);
+    $("#search form select").prop("disabled", true);
+
+    // Send search data to server
+    $.ajax({
+      type: "POST",
+      url: $(this).attr("action"),
+      data: $(this).serialize() + "&searchTerm=" + searchTerm,
+      success: function (response) {
+        console.log(response);
+
+	// Get length of search data array and add to results table
+	var length = JSON.parse(response).length;
+	for (var i = 0; i < length; i++)
+	{
+		// Add book info to table
+		var row = "<tr>";
+		for (var j = 0; j < JSON.parse(response)[i].length; j++)
+		{
+			row += "<td>" + JSON.parse(response)[i][j] + "</td>";
+		}
+		row += "</tr>";
+		$("#results tbody").append(row);
+	}
+	
+	$("#results").trigger("update");
+      },
+    });
+    return false;
+  });
+
+  // Sort search results on request
+  $("#results").tablesorter(); // Utilize JQuery plugin
+  $("#results th").click(function () {
+    // Sort table by clicked column
+    $("#sort").remove();
+    $(this).prepend("<span id='sort'>></span>");
+  });
+
+  // Get list of books from server
+  if ($("select[name=book]").length) {
+ 	$.ajax({
+      	    type: "POST",
+      	    url: "server.php",
+      	    data: "getBooks",
+      	    success: function (response) {
+        	console.log(response);
+
+		// Get length of book list and add book options
+		var length = JSON.parse(response).length;
+		for (var i = 0; i < length; i++)
+		{
+			// Get book title and author
+			var title = JSON.parse(response)[i][0];
+			var author = JSON.parse(response)[i][1];
+			var isbn = JSON.parse(response)[i][2];
+
+			// Add book option
+        		$("select[name=book]").append("<option value=" + isbn + ">" + title + " - " + author + "</option>");
+		}
+      		},
+    	    });
+  }
+
+  // Check for order form submissions
+  $("#order form").submit(function () {
+    // Show success message and disable new submissions
+    $("#success").show();
+    $(this).find(":submit").prop("disabled", true);
+
+    // Send order data to server
+    $.ajax({
+      type: "POST",
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+      success: function (response) {
+        console.log(response);
+      },
+    });
+    return false;
+  });
+
+  // Check for review form submissions
+  $("#review form").submit(function () {
+    // Show success message and disable new submissions
+    $("#success").show();
+    $(this).find(":submit").prop("disabled", true);
+
+    // Send review data to server
+    $.ajax({
+      type: "POST",
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+      success: function (response) {
+        console.log(response);
+      },
+    });
+	return false;
+  });
+
+  // Check if user checks order history
+  if (window.location.href.indexOf("history") >= 0) {
+    // Request history from server
+    $.ajax({
+      type: "POST",
+      url: "server.php",
+      data: "history",
+      success: function (response) {
+        console.log(response);
+
+	// Get length of history data array and add to history table
+        var length = JSON.parse(response).length;
+	for (var i = 0; i < length; i++)
+        {
+                // Add order info to table
+                var row = "<tr>";
+                for (var j = 0; j < JSON.parse(response)[i].length; j++)
+                {
+                        row += "<td>" + JSON.parse(response)[i][j] + "</td>";
                 }
-            }
-
-	    $("#features").show(); // Show user options in navigation bar
-            $("#login").hide(); // Hide log in content from main page
-            $("#choices").show(); // Show user options on main page
-            $("#loginNav").hide(); // Hide log in link
-            $("#signupNav").hide(); // Hide sign up link
-            $("#logoutNav").show(); // Show log out link
-
-            // Redirect to home if trying to sign up when already logged in
-            if (window.location.href.indexOf("signup") >= 0) {
-                window.location.href = "index.html";
-            }
+                row += "</tr>";
+                $("#historyTable tbody").append(row);
         }
-        else {
-            // Redirect to home if trying to use options when logged out
-            if (window.location.href.indexOf("search") >= 0
-                || window.location.href.indexOf("order") >= 0
-                || window.location.href.indexOf("review") >= 0
-                || window.location.href.indexOf("history") >= 0
-                || window.location.href.indexOf("manage") >= 0) {
-                window.location.href = "index.html";
-            }
-        }
+
+        $("#historyTable").trigger("update");
+
+      },
     });
+  }
 
-    // Check for log in form submissions
-    $("#login form").submit(function () {
-        // Redirect to home while still sending data to server
-        $.ajax({
-            type: "POST",
-            url: $(this).attr("action"),
-            data: $(this).serialize(),
-            success: function () {
-                window.location.href = "index.html";
-            }
-        });
-        return false;
+	// Sort order history on request
+  $("#historyTable").tablesorter(); // Utilize JQuery plugin
+  $("#historyTable th").click(function () {
+	console.log("!");
+    // Sort table by clicked column
+    $("#sort").remove();
+    $(this).prepend("<span id='sort'>></span>");
+  });
+
+
+  // Check if manage task is selected
+  $("#manage select[name=task]").click(function () {
+    // Show corresponding form and hide all others
+    var form = "#" + $(this).find("option:selected").val();
+    $(form).show();
+    $("#manage form").not(form).hide();
+  });
+
+  // Check for manage form submissions
+  $("#manage form").submit(function () {
+    // Show success message, disable new submissions, and disable changing task
+    $("#success").show();
+    $(this).find(":submit").prop("disabled", true);
+    $("#manage select[name=task]").prop("disabled", true);
+
+    // Send task data to server
+    $.ajax({
+      type: "POST",
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+      success: function (response) {
+        console.log(response);
+      },
     });
+    return false;
+  });
 
-    // Check for sign up form submissions
-    $("#signup form").submit(function () {
-        // Redirect to home while still sending data to server
-        $.ajax({
-            type: "POST",
-            url: $(this).attr("action"),
-            data: $(this).serialize(),
-            success: function () {
-                window.location.href = "index.html";
-            }
-        });
-        return false;
+  // Check if user logs out
+  $("#logoutNav a").click(function () {
+    $.ajax({
+      type: "POST",
+      url: "server.php",
+      data: "logout",
+      success: function (response) {
+        console.log(response);
+
+        window.location.href = "index.html";
+      },
     });
-
-    // Check for search form submissions
-    $("#search form").submit(function (e) {
-        $.ajax({
-            type: "POST",
-            url: $(this).attr("action"),
-            data: $(this).serialize(),
-            success: function (response) {
-                console.log(response);
-
-                // Show results table
-                $("#results").show();
-
-                // Highlight search term in results table
-                var searchTerm = "#" + $(this).find("option:selected").val();
-                $("#results " + searchTerm).addClass("text-primary");
-
-                // Disable new submissions and changing search term
-                $(this).find(":submit").prop("disabled", true);
-                $("#search form select").prop("disabled", true);
-            }
-        });
-        return false;
-    });
-
-    // Sort search results on request
-    $("#results").tablesorter(); // Utilize JQuery plugin
-    $("#results th").click(function () {
-        // Sort table by clicked column
-        $("#sort").remove();
-        $(this).prepend("<span id='sort'>></span>");
-    });
-
-    // Check for order form submissions
-    $("#order form").submit(function () {
-        // Show success message and disable new submissions
-        $("#success").show();
-        $(this).find(":submit").prop("disabled", true);
-    });
-
-    // Check for review form submissions
-    $("#review form").submit(function () {
-        // Show success message and disable new submissions
-        $("#success").show();
-        $(this).find(":submit").prop("disabled", true);
-    });
-
-    // Check if user checks history
-    $("#historyNav a").click(function () {
-        // Request history from server
-        $.post("server.php", { history: true });
-    });
-
-    // Check if manage task is selected
-    $("#manage select[name=task]").click(function () {
-        // Show corresponding form and hide all others
-        var form = "#" + $(this).find("option:selected").val();
-        $(form).show();
-        $("#manage form").not(form).hide();
-    });
-
-    // Check for manage form submissions
-    $("#manage form").submit(function () {
-        // Show success message, disable new submissions, and disable changing task
-        $("#success").show();
-        $(this).find(":submit").prop("disabled", true);
-        $("#manage select[name=task]").prop("disabled", true);
-    });
-
-    // Check if user logs out
-    $("#logoutNav a").click(function () {
-        // Tell server to end session for user logging out
-        $.post("server.php", { logout: true });
-    });
-}
+    return false;
+  });
+};
--- web-app/server.php
+++ web-app/server.php
@@ -1,103 +1,380 @@
 <?php
+	// Team 16 Bookstore (server.php)
+	// Authors: Ben Holzhauer (PHP), Nicholas Early (SQL)
+	// Class: CS 405G-001
+	
         // Allow access to server
         header("Access-Control-Allow-Origin: *");
 
-        // Start user session
-        session_start();
+	// Set up database connection
+	$conn = mysqli_connect("mysql.cs.uky.edu", "bgho224", "team16", "bgho224");	
+        
+	// Start user session if not active
+        if (!$_SESSION)
+	{
+		session_start();
+	}
 
         // Handle post requests
 	if ($_POST)
         {
-                // Check for user log in or sign up
-                if (isset($_POST["loginForm"]) || isset($_POST["signupForm"]))
+		// Check for user log in
+		if (isset($_POST["loginForm"]))
+		{
+			$email = $_POST["email"];
+			$password = $_POST["password"];
+
+			$userResults = mysqli_query($conn, "SELECT uid, role FROM users WHERE email='{$email}' AND password='{$password}'");
+                        if ($userResults)
+                        {
+                                $row = mysqli_fetch_row($userResults);
+                                $_SESSION["uid"] = $row[0];
+                                $_SESSION["role"] = $row[1];
+
+                                mysqli_free_result($userResults);
+
+                                echo "USER LOGGED IN";
+			}
+		}
+
+                // Check for user sign up
+                if (isset($_POST["signupForm"]))
                 {
-			// Get role of user from database 
-                        $_SESSION["role"] = "manager";
-                }
+			$email = $_POST["email"];
+			$password = $_POST["password"];
+			$role = $_POST["role"];
+			$fname = $_POST["fname"];
+			
+			$mname = "'" . $_POST["mname"] . "'";
+			if (!$_POST["mname"])
+			{
+				$mname = "null";
+			}
+
+			$lname = $_POST["lname"];
+			
+			$age = $_POST["age"];
+			if (!$age)
+			{
+				$age = "null";
+			}			
+
+			$gender = $_POST["gender"];
+
+			$signupResults = mysqli_query($conn, "INSERT INTO users(email, password, role, fname, mname, lname, age, gender) VALUES ('{$email}', '{$password}', '{$role}', '{$fname}', {$mname}, '{$lname}', {$age}, '{$gender}')");
+			if ($signupResults)
+			{
+				$uidResults = mysqli_query($conn, "SELECT MAX(uid) FROM users");
+				if ($uidResults)
+				{
+					$row = mysqli_fetch_row($uidResults);
+					$_SESSION["uid"] = $row[0];
+                        		$_SESSION["role"] = $role;
+
+					mysqli_free_result($uidResults);
+				}
+			
+				echo "USER SIGNED UP";
+
+				mysqli_free_result($signupResults);
+			}
+		}
 
                 // Handle making searches
 		if (isset($_POST["searchForm"]))
 		{
-                        echo var_dump($_POST);
-                        
-                        // Echo array of search data
+			$term = $_POST["searchTerm"];
+			$words = $_POST["searchWords"];
+			
+			if ($term == "b.price")
+			{
+				$searchResults = mysqli_query($conn, "SELECT b.isbn, b.name, a.author, b.publisher, b.pubDate, b.subject, b.price, b.quantity, k.keyword FROM books AS b, authors AS a, book_keywords AS k WHERE b.price <= {$words} AND b.isbn=a.isbn AND b.isbn=k.isbn");
+			}
+			else
+			{
+                        	$searchResults = mysqli_query($conn, "SELECT b.isbn, b.name, a.author, b.publisher, b.pubDate, b.subject, b.price, b.quantity, k.keyword FROM books AS b, authors AS a, book_keywords AS k WHERE {$term} LIKE '%{$words}%' AND b.isbn=a.isbn AND b.isbn=k.isbn");
+			}
+
+			if ($searchResults)
+			{
+				$books = [];
+				while ($row = mysqli_fetch_row($searchResults))
+				{
+					$books[] = $row;
+				}
+
+				echo json_encode($books);
+	
+				mysqli_free_result($searchResults);
+			}
                 }
+
+		// Check for list of book request
+		if (isset($_POST["getBooks"]))
+		{
+			$bookResults = mysqli_query($conn, "SELECT b.name, a.author, b.isbn FROM books AS b, authors AS a WHERE b.isbn=a.isbn");
+			if ($bookResults)
+			{
+				$books = [];
+				while ($row = mysqli_fetch_row($bookResults))
+				{
+					$books[] = [$row[0], $row[1], $row[2]];
+				}
+
+				echo json_encode($books);
+
+				mysqli_free_result($bookResults);
+			}
+		}
                 
                 // Handle making orders
                 if (isset($_POST["orderForm"]))
 		{
-                        echo var_dump($_POST);
-                        
-                        // Process order
-                }
+                        $uid = $_SESSION["uid"];
+			$isbn = $_POST["book"];
+			$ccNumber = $_POST["ccNumber"];
+			$quantity = $_POST["quantity"];
+			$billingAddr = $_POST["billingAddr"];
+			$shippingAddr = $_POST["shippingAddr"];
+
+			$priceResults = mysqli_query($conn, "SELECT price FROM books WHERE isbn={$isbn}");
+			if ($priceResults)
+			{
+				$row = mysqli_fetch_row($priceResults);
+				$cost = $row[0] * $quantity;
+			
+				mysqli_free_result($priceResult);
+			
+				$orderResults = mysqli_query($conn, "INSERT INTO orders (uid, isbn, ccNumber, cost, status, quantity, billingAddr, orderDate, shippingAddr) VALUES ({$uid}, {$isbn}, {$ccNumber}, {$cost}, 'Pending', {$quantity}, '{$billingAddr}', NOW(), '{$shippingAddr}')");
+				if ($orderResults)
+				{
+					echo "ORDER PROCESSED";
+
+					mysqli_free_result($orderResults);
+				}
+                	}
+		}
                 
                 // Handle writing reviews
                 if (isset($_POST["reviewForm"]))
 		{
-                        echo var_dump($_POST);
-                        
-                        // Process review
+                        $uid = $_SESSION["uid"];
+			$isbn = $_POST["book"];
+			$rating = $_POST["rating"];
+			$review = $_POST["review"];
+
+			$reviewResults = mysqli_query($conn, "INSERT INTO reviews (uid, isbn, rating, review) VALUES ({$uid}, {$isbn}, {$rating}, '{$review}')");
+			if ($reviewResults)
+			{
+				echo "REVIEW WRITTEN";
+			
+				mysqli_free_result($reviewResults);
+			}
                 }
                 
                 // Handle checking order history
                 if (isset($_POST["history"]))
 		{
-                        echo var_dump($_POST);
-                        
-                        // Echo array of order history
+                        $uid = $_SESSION["uid"];
+
+                        $historyResults = mysqli_query($conn, "SELECT o.orderDate, o.isbn, b.name, a.author, o.quantity, o.cost, o.shippingAddr, o.status, o.ccNumber, o.billingAddr FROM orders AS o, books AS b, authors AS a WHERE o.uid={$uid} AND o.isbn=b.isbn AND b.isbn=a.isbn");
+			if ($historyResults)
+                        {
+                                $history = [];
+                                while ($row = mysqli_fetch_row($historyResults))
+                                {
+                                        $history[] = $row;
+                                }
+
+                                echo json_encode($history);
+
+                                mysqli_free_result($historyResults);
+                        }
                 }
                 
                 // Handle management book adding
                 if (isset($_POST["addBook"]))
                 {
-                        echo var_dump($_POST);
+                        $isbn = $_POST["isbn"];
+                        $name = $_POST["name"];
+			$author = $_POST["author"];
+			$publisher = $_POST["publisher"];
+			$pubDate = $_POST["pubDate"];
+			$subject = $_POST["subject"];
+			
+			$summary = "'" . $_POST["summary"] . "'";
+			if (!$_POST["summary"])
+                        {
+                                $summary = "null";
+                        }
 
-                        // Process action
+			$price = $_POST["price"];
+			$quantity = $_POST["quantity"];
+		
+                        $addBookResults = mysqli_query($conn, "INSERT INTO books (isbn, name, publisher, pubDate, subject, summary, price, quantity) VALUES ({$isbn}, '{$name}', '{$publisher}', '{$pubDate}', '{$subject}', {$summary}, {$price}, {$quantity})");
+                        if ($addBookResults)
+                        {
+                                mysqli_free_result($addBookResults);
+
+				$addAuthorResults = mysqli_query($conn, "INSERT INTO authors (author, isbn) VALUES ('{$author}', {$isbn})");
+				if ($addAuthorResults)
+				{
+					echo "BOOK ADDED";
+
+					mysqli_free_result($addAuthorResults);
+				}
+			}
                 }
 
                 // Handle management book updating
                 if (isset($_POST["updateBook"]))
                 {
-                        echo var_dump($_POST);
+                	$oldIsbn = $_POST["book"];
 
-                        // Process action
-                }
+			$isbn = $_POST["isbn"];
+			if ($isbn)
+			{
+				$isbn = "isbn=" . $isbn . ", ";
+			}
+
+			$name = $_POST["name"];
+			if ($name)
+			{
+				$name = "name='" . $name . "', ";
+			}
+
+			$author = $_POST["author"];
+			if ($author)
+			{
+				$author = "author='" . $author . "'";
+			}
+
+			$publisher = $_POST["publisher"];
+			if ($publisher)
+			{
+				$publisher = "publisher='" . $publisher . "', ";
+			}
+
+                        $pubDate = $_POST["pubDate"];
+			if ($pubDate)
+			{
+				$pubDate = "pubDate=" . $pubDate . ", ";
+			}
+
+                        $subject = $_POST["subject"];
+			if ($subject)
+			{
+				$subject = "subject='" . $subject . "', ";
+			}
+
+			$summary = $_POST["summary"];
+			if ($summary)
+			{
+				$summary = "summary='" . $summary . "', ";
+			}
+
+			$price = $_POST["price"];
+			if ($price)
+			{
+				$price = "price=" . $price . ", ";
+			}
+
+			$quantity = $_POST["quantity"];
+			if ($quantity)
+			{
+				$quantity = "quantity=" . $quantity . ", ";
+			}
+
+			$updateBookResults = mysqli_query($conn, "UPDATE books SET {$isbn}{$name}{$publisher}{$pubDate}{$subject}{$summary}{$price}{$quantity}language=null WHERE isbn={$oldIsbn}");
+                	if ($updateBookResults)
+			{
+				echo "BOOK UPDATED";
+
+				mysqli_free_result($updateBookResults);
+			}
+
+			if ($author)
+			{
+				$updateAuthorResults = mysqli_query($conn, "UPDATE authors SET {$isbn}{$author} WHERE isbn={$oldIsbn}");
+				if ($updateAuthorResults)
+				{
+					echo "AUTHOR UPDATED";
+	
+					mysqli_free_result($updateAuthorResults);
+				}
+			}
+		}
 
                 // Handle management book deletion
                 if (isset($_POST["deleteBook"]))
                 {
-                        echo var_dump($_POST);
+                	$isbn = $_POST["book"];
 
-                        // Process action
+			$deleteAuthorResults = mysqli_query($conn, "DELETE FROM authors WHERE isbn={$isbn}");
+			if ($deleteAuthorResults)
+			{
+				mysqli_free_result($deleteAuthorResults);
+
+				$deleteBookResults = mysqli_query($conn, "DELETE FROM books WHERE isbn={$isbn}");
+                       		if ($deleteBookResults)
+                       		{
+					echo "BOOK DELETED";
+					
+					mysqli_free_result($deleteBookResults);
+				}
+				
+			}
                 }
 
                 // Handle management keyword adding
                 if (isset($_POST["addKeyword"]))
                 {
-                        echo var_dump($_POST);
+                        $isbn = $_POST["book"];
+                        $keyword = $_POST["keyword"];
+			
+			$addKeywordResults = mysqli_query($conn, "INSERT INTO book_keywords(isbn, keyword) VALUES ({$isbn}, '{$keyword}')");
+                        if ($addKeywordResults)
+                        {
+				echo "KEYWORD ADDED";
 
-                        // Process action
+                                mysqli_free_result($addKeywordResults);
+                        }
                 }
 
                 // Handle management keyword updating
                 if (isset($_POST["updateKeyword"]))
                 {
-                        echo var_dump($_POST);
+                        $oldKeyword = $_POST["oldKeyword"];
+			$newKeyword = $_POST["newKeyword"];
 
-                        // Process action
+			$updateKeywordResults = mysqli_query($conn, "UPDATE book_keywords SET keyword='{$newKeyword}' WHERE keyword='{$oldKeyword}'");
+			if ($updateKeywordResults)
+			{
+				echo "KEYWORD UPDATED";
+		
+				mysqli_free_result($updateKeywordResults);
+			}
                 }
 
                 // Handle management keyword deletion
                 if (isset($_POST["deleteKeyword"]))
                 {
-                        echo var_dump($_POST);
+                        $keyword = $_POST["keyword"];
 
-                        // Process action
+			$deleteKeywordResults = mysqli_query($conn, "DELETE FROM book_keywords WHERE keyword='{$keyword}'");
+			if ($deleteKeywordResults)
+			{
+				echo "KEYWORD DELETED";
+
+				mysqli_free_result($deleteKeywordResults);
+			}
                 }
 
                 // Check if user logs out
                 if (isset($_POST["logout"]))
                 {
+			echo "USER LOGGED OUT";
+
                         // End and destroy session
                         session_unset();
                         session_destroy();
@@ -105,7 +382,10 @@
         }
 	else
 	{
-		// Get role of logged in user
-		echo $_SESSION["role"];
+		// Get UID and role of current user
+		echo json_encode([$_SESSION["uid"], $_SESSION["role"]]);
 	}
+
+	// Close database connection
+	$conn->close();
 ?>
